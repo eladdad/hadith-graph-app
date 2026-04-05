a82b8bc05b071be17bbaf1014be6dfd7da9d743a
Ibrahim Hassan <eeladdad@gmail.com>
2026-04-05 19:55:32 +0000

    Add github pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 ﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/hadith-graph-app/' : '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
     include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx'],
   },
-});
+}));
