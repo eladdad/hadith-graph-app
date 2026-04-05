4e77dbc0bef87736d66b329bac6c13289ea795df
Ibrahim Hassan <eeladdad@gmail.com>
2026-04-05 21:49:16 +0000

    Change vite base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/hadith-graph-app/' : '/',
+  base: '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
